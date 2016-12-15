cbaa955b95b73850bb6390372e6ff856fa4d0ecb
Odiki <rodion.zinovyev@gmail.com>
2016-12-15 14:28:54 +0200

    Recipe Item

--- src/app/recepies/recipe-list/recepe-item.component.ts
+++ src/app/recepies/recipe-list/recepe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Recipe} from '../recipe';
 
 @Component({
   selector: 'rb-recepe-item',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecepeItemComponent implements OnInit {
 
+@Input() recipe : Recipe;
+recipeId : number;
   constructor() { }
 
   ngOnInit() {
--- src/app/recepies/recipe-list/recipe-list.component.ts
+++ src/app/recepies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe';
 
 @Component({
   selector: 'rb-recipe-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes : Recipe[] = [];
+
 
   constructor() { }
 
