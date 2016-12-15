4a54ff3dbed8527301927c3b9d21350e063ae7bb
Odiki <rodion.zinovyev@gmail.com>
2016-12-15 15:38:31 +0200

    Model and List Component

--- src/app/recepies/recepies.component.ts
+++ src/app/recepies/recepies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeListComponent } from '/Users/rodionzinovyev/GitHub/Recipe-Book-App/src/app/recepies/recipe-list/recipe-list.component';
 
 @Component({
   selector: 'rb-recepies',
--- src/app/recepies/recipe-list/recipe-list.component.ts
+++ src/app/recepies/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from '../recipe';
+import { Recipe } from '../recipe';
+import {RecepeItemComponent} from './recepe-item.component'
 
 @Component({
   selector: 'rb-recipe-list',
@@ -8,7 +9,7 @@ import {Recipe} from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 recipes : Recipe[] = [];
-
+recipe = new Recipe('Dummy', 'Dummy', 'http://i.dailymail.co.uk/i/pix/2015/08/12/01/1AF9646D000005DC-3194442-image-a-21_1439339842820.jpg');
 
   constructor() { }
 
