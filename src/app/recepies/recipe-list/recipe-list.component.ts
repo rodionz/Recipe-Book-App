9987efa5fdd6c4062113cce58d8c1c2330f541b6
Odiki <rodion.zinovyev@gmail.com>
2016-12-20 13:50:12 +0200

    Ingredients list which ngFor

--- src/app/recepies/recipe-list/recipe-list.component.ts
+++ src/app/recepies/recipe-list/recipe-list.component.ts
@@ -8,9 +8,12 @@ import {RecepeItemComponent} from './recepe-item.component'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes : Recipe[] = [];
+recipes : Recipe[] = [
+  new Recipe('Schnitzel', 'Very tasty', 'http://sm.askmen.com/askmen_in/photo/w/wienerschnitzel%3A-vienna,-austria/wienerschnitzel%3A-vienna,-austria_k22d.jpg', []),
+  new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+];
 @Output()  recipeSelected = new EventEmitter<Recipe>()
-recipe = new Recipe('Dummy', 'Dummy', 'http://i.dailymail.co.uk/i/pix/2015/08/12/01/1AF9646D000005DC-3194442-image-a-21_1439339842820.jpg');
+
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../shared/ingredient";
 
 @Component({
   selector: 'rb-shopping-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class ShoppingListComponent implements OnInit {
+
+items: Ingredient[] = [];
 
   constructor() { }
 
