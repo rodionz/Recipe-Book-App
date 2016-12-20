d3df825dc9b0a8f0db8e00e7edde3bbfda60cf7b
Odiki <rodion.zinovyev@gmail.com>
2016-12-20 14:10:22 +0200

    Custom DropDownDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecepeItemComponent } from './recepies/recipe-list/recepe-item.componen
 import { RecipeDetailComponent } from './recepies/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
+import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.comp
     RecepeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
-    ShoppingListAddComponent
+    ShoppingListAddComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
