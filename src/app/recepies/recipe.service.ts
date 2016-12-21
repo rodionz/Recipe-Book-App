83450233dc1ba05ea968c7f3c906ded63e0d408b
Odiki <rodion.zinovyev@gmail.com>
2016-12-21 15:46:25 +0200

    Cross Service Communication

--- src/app/recepies/recipe.service.ts
+++ src/app/recepies/recipe.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import {Recipe} from './recipe';
+import {Ingredient} from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
 
 private recipes : Recipe[] = [
-  new Recipe('Schnitzel', 'Very tasty', 'http://sm.askmen.com/askmen_in/photo/w/wienerschnitzel%3A-vienna,-austria/wienerschnitzel%3A-vienna,-austria_k22d.jpg', []),
+  new Recipe('Schnitzel', 'Very tasty', 'http://sm.askmen.com/askmen_in/photo/w/wienerschnitzel%3A-vienna,-austria/wienerschnitzel%3A-vienna,-austria_k22d.jpg', 
+  [
+     new Ingredient('French Fries', 2),
+     new Ingredient ('Meat',1)
+  ]),
   new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
 ];
   constructor() { }
